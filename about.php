acd7b5f531d873e11e6032858ceb7ede4c8dbc3e
laksalks <123230235@student.upnyk.ac.id>
2026-09-11 16:45:59 +0700

    Add cache-busting query parameter for style.css

--- about.php
+++ about.php
@@ -16,7 +16,7 @@ $loggedIn = isset($_SESSION['id']);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=2">
 </head>
 <body>
 
